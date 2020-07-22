c65fdc395bc1a4623687dd3dd783f06fd08ef0ef
BingJiaChen <55092297+BingJiaChen@users.noreply.github.com>
2020-07-22 15:01:38 +0800

    Delete ex7.py

--- exercise/ex7.py
+++
@@ -1,6 +0,0 @@
-print('start to prewiit...')
-result = np.ones(img.shape) * 255 # new array for prewiit 
-for i in range(1, h-1):
-    for j in range(1, w-1):
-        ##TODO
-        result[i, j] = np.array([G, G, G])
