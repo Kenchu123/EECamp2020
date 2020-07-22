dd387a9e2774b23bc62e3497de7099b8cc2f7838
yingchiaochen <yingchiao.chen@gmail.com>
2020-07-22 14:16:20 +0800

    modify ex5 and ppt

--- answer/ans5.py
+++ answer/ans5.py
@@ -1,21 +1,67 @@
 ##########################################################################################
 # Example 5:
 # 
-# Write a function that can convert a decimal number to a bianry number.
+# Write a function that can convert a bill to change.(找零機)
 # 
 ##########################################################################################
 
 
 def convert(N):
-    s = []
-    while N / 2 != 0:
-        s.insert(0, N % 2)
-        N = N // 2
+    num_50 = 0
+    num_10 = 0
+    num_5 = 0
+    num_1 = 0
 
-    return s
+    num_50 = N // 50
+    N = N % 50
 
-n = input("Please enter a decimal number: ")
-l = [str(i) for i in convert(int(n))]
-print(''.join(l))
+    num_10 = N // 10
+    N = N % 10
+
+    num_5 = N // 5
+    N = N % 5
+
+    num_1 = N // 1
+    N = N % 1
+    
+    print(f"50: {num_50}")
+    print(f"10: {num_10}")
+    print(f"5: {num_5}")
+    print(f"1: {num_1}")
+
+def convert_list(N):
+    num = [0, 0, 0, 0]
+    coin = [50, 10, 5, 1]
+
+    for index, val in enumerate(coin):
+        num[index] = N // val
+        N = N % val
+    
+    print(f"50: {num[0]}")
+    print(f"10: {num[1]}")
+    print(f"5: {num[2]}")
+    print(f"1: {num[3]}")
+
+def convert_dict(N):
+    num = {50: 0, 10: 0, 5: 0, 1:0}
+    coin = [50, 10, 5, 1]
+
+    for i in coin:
+        num[i] = N // i
+        N = N % i
+
+    print(f"50: {num[50]}")
+    print(f"10: {num[10]}")
+    print(f"5: {num[5]}")
+    print(f"1: {num[1]}")
+
+
+n = input("Please enter a number: ")
+# convert(int(n))
+# convert_list(int(n))
+convert_dict(int(n))
+
+
+
 
 
--- exercise/ex5.py
+++ exercise/ex5.py
@@ -1,19 +1,28 @@
 ##########################################################################################
 # Example 5:
 # 
-# Write a function that can convert a decimal number to a bianry number.
+# Write a function that can convert a bill to change.(找零機)
 # 
 ##########################################################################################
 
 
 def convert(N):
+    num_50 = 0
+    num_10 = 0
+    num_5 = 0
+    num_1 = 0
+
     ### TODO:
 
     ###
 
-n = input("Please enter a decimal number: ")
-l = [str(i) for i in convert(int(n))]
-print(''.join(l))
+    print(f"50: {num_50}")
+    print(f"10: {num_10}")
+    print(f"5: {num_5}")
+    print(f"1: {num_1}")
+
+n = input("Please enter a number: ")
+convert(int(n))
 
 
 
